fefc05b41b26886a3c7c4a35c7ef03f1f4f2d028
NaheedRayan <naheed28ray@protonmail.com>
2021-03-03 19:52:28 +0600

    tweaked for deploying to heroku

--- app.py
+++ app.py
@@ -44,4 +44,6 @@ def handle_leave_room_event(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    # socketio.run(app,host = "192.168.43.151" , port= 5000, debug=True )
+    # socketio.run(app,host = "0.0.0.0" , port= 5000, debug=True )
+    socketio.run(app)
